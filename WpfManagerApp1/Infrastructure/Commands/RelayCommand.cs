961a29fc4299d2c2f1adf791de219c44db81a172
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2024-01-27 22:29:36 +0300

    Созданы классы команд

---
+++ WpfManagerApp1/Infrastructure/Commands/RelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfManagerApp1.Infrastructure.Commands
+{
+    internal class RelayCommand : Command
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+        public override bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => execute?.Invoke(parameter);
+    }
+}
--- WpfManagerApp1/ViewModel/MainWindowVM.cs
+++ WpfManagerApp1/ViewModel/MainWindowVM.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfManagerApp1.Infrastructure.Commands;
 using WpfManagerApp1.ViewModel.Base;
 
 namespace WpfManagerApp1.ViewModel
 {
     internal class MainWindowVM : ViewModelBase
     {
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecute(object parameter) => true;
+        private void OnCloseApplicationCommandExecuted(object parameter)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #endregion
+
+        #region Свойства
+
+        private 
+
+        #region Title - название окна
         private string _title = "Планировщик";
         /// <summary>
         /// Заголовок окна
@@ -20,6 +41,18 @@ namespace WpfManagerApp1.ViewModel
             {
                 Set(ref _title, value);
             }
+        } 
+        #endregion
+
+        #endregion
+
+        public MainWindowVM()
+        {
+            #region Команды
+
+            CloseApplicationCommand = new RelayCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
         }
     }
 }
