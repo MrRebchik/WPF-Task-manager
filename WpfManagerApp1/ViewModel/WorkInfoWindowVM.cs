450c4263643317d548fe1c1760b9f7e8e16114b0
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2024-02-12 19:17:52 +0300

    Создал окно WorkInfo

---
+++ WpfManagerApp1/ViewModel/WorkInfoWindowVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfManagerApp1.Model;
+using WpfManagerApp1.ViewModel.Base;
+
+namespace WpfManagerApp1.ViewModel
+{
+    internal class WorkInfoWindowVM : ViewModelBase
+    {
+        private Work currentWork;
+
+        public Work CurrentWork 
+        { 
+            get => currentWork; 
+            set => Set(ref currentWork,value); 
+        }
+        public string CurrentWorkType
+        {
+            get => CurrentWork.GetType().ToString();
+        }
+
+        public WorkInfoWindowVM(Work work)
+        {
+            this.currentWork = work;
+        }
+    }
+}
