e9b1ccec4941d87ccf55bd63099070aa22a03082
Penulltimo <38166546+MrRebchik@users.noreply.github.com>
2024-02-14 20:31:49 +0300

    Создавал MainWorkList UserControl

---
+++ WpfManagerApp1/ViewModel/MainWorkListVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using WpfManagerApp1.ViewModel.Base;
+using System.Collections.ObjectModel;
+using WpfManagerApp1.Model;
+
+namespace WpfManagerApp1.ViewModel
+{
+    internal class MainWorkListVM : ViewModelBase
+    {
+        public ObservableCollection<Work> WorksCollection { get; set; }
+    }
+}
--- WpfManagerApp1/Views/UserControls/MainWorkList.xaml.cs
+++ WpfManagerApp1/Views/UserControls/MainWorkList.xaml.cs
@@ -24,5 +24,13 @@ namespace WpfManagerApp1.Views.UserControls
         {
             InitializeComponent();
         }
+        private void Work_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                sender is FrameworkElement frameworkElement)
+            {
+                DragDrop.DoDragDrop(frameworkElement, new DataObject(DataFormats.Serializable, frameworkElement.DataContext), DragDropEffects.Copy);
+            }
+        }
     }
 }
